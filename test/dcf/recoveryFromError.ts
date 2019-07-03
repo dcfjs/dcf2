876ecb7244a1d468b28f81a55709d44a67c067df
DengYun <dengyun@meideng.net>
2019-07-03 13:50:58 +0800

    add error recovery test

---
+++ test/dcf/recoveryFromError.ts
@@ -0,0 +1,42 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { releaseAll, autoRelease } from '@dcfjs/common/autoRelease';
+import { createMasterServer } from '@dcfjs/master';
+import { createLocalWorker } from '@dcfjs/worker';
+import { Context, createContext } from '@dcfjs/client/Context';
+import { expect } from 'chai';
+import { RDD } from '@dcfjs/client';
+
+chai.use(chaiAsPromised);
+
+describe('MapReduce With local worker', () => {
+  let dcc: Context;
+
+  before(async () => {
+    const server = await createMasterServer();
+    autoRelease(() => server.close());
+
+    for (let i = 0; i < 4; i++) {
+      autoRelease(await createLocalWorker(server.endpoint));
+    }
+
+    dcc = await createContext(server.endpoint);
+    autoRelease(() => dcc.close());
+  });
+
+  after(async () => {
+    await releaseAll();
+    context = null!;
+  });
+
+  it('Test dcf function error', async () => {
+    expect(
+      dcc
+        .range(0, 100, 4)
+        .map(a => {
+          throw new Error('some error');
+        })
+        .count(),
+    ).to.be.rejectedWith();
+  });
+});
